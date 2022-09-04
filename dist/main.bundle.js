fb289ff7f233fbc99482544c48d4d9e1b7551db5
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-09-04 19:02:33 +0530

    setup webpack config typescript

---
+++ dist/main.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+(self["webpackChunkhowyoudoin"] = self["webpackChunkhowyoudoin"] || []).push([["main"],{
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+
+alert("tell me the truth");
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.ts"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLEtBQUssQ0FBQyxtQkFBbUIsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2hvd3lvdWRvaW4vLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQoXCJ0ZWxsIG1lIHRoZSB0cnV0aFwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+alert("tell me the truth")
