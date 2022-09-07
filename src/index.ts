0fbc1e3eb66b95bcdf18ad4ac2e00ec258c3d672
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-09-07 09:44:54 +0530

    open todo-form with add button

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import "./style.scss"
 
-
+let addBtn = document.querySelector(".add-task")
 let mainView = document.querySelector(".main-view")
+let todoContainer = document.querySelector(".todo-container")
+let inputForm  = document.querySelector(".input-form")!
+inputForm?.remove();
+
+const createForm = () => {
+  const input = document.createElement("input")
+  
+}
 
 const addTask  = (e: Event) => {
-  let task = document.createElement("div");
-  task.textContent = "testing tasks"
-  mainView?.appendChild(task)
+  // let task = document.createElement("div");
+  // task.classList.add("todo-item")
+  // task.textContent = "testing tasks"
+  addBtn?.remove();
+  mainView?.appendChild(inputForm);
+}
+
+
+const removeTask = (e: Event) => {
+
 }
 
 //problem 1
-let addBtn = document.querySelector(".add-task")
 addBtn?.addEventListener("click", addTask)
 
 
