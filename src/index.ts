d85e8a581f302b7b809896faece5c09f74229396
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-09-08 12:55:47 +0530

    add checked on click feature todo

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const todoContainer = document.querySelector(".todo-container")
 const inputForm  = document.querySelector(".input-form")
 const closeFormBtn = document.querySelector(".close-form")
 const pushTaskBtn = document.querySelector(".push-task")
+const todoItems = document.querySelectorAll("todo-items")
+
 const inputElm : any = inputForm?.firstElementChild
 
 
@@ -25,16 +27,60 @@ const closeForm = () => {
   openFormBtn && mainView?.appendChild(openFormBtn);
 }
 
-const addToDoItem = () => {
+const removeTodoItem = () => {
+
+}
+
+const completeTodoItem = (e: any) => {
+  e.target.classList.toggle("checked")
+  e.target.nextSibling.classList.toggle("todo-item-checked");
+  //alert("whoop")
+}
+
+const favoriteTodoItem = () => {
+
+}
+
+const createToDoElm = (): HTMLDivElement => {
   let task = document.createElement("div");
-  task.classList.add("todo-item")
-  task.textContent = inputElm.value
-  todoContainer?.appendChild(task);
-  closeForm()
+  task.classList.add("todo-items");
+
+  let checkIcon = document.createElement("span");
+  checkIcon.classList.add("material-icons-round", "check", "todo-btn");
+  checkIcon.textContent = "check_circle";
+  checkIcon.addEventListener("click", completeTodoItem)
+  task.appendChild(checkIcon)
+
+  let text = document.createElement("span");
+  text.classList.add("todo-item-text");
+  text.textContent = inputElm.value;
+  task.appendChild(text)
+
+  let starIcon = document.createElement("span");
+  starIcon.classList.add("material-icons-round", "star", "todo-btn");
+  starIcon.textContent = "star_border";
+  task.appendChild(starIcon)
+
+  let cancelIcon = document.createElement("span");
+  cancelIcon.classList.add("material-icons-round", "cancel", "todo-btn");
+  cancelIcon.textContent = "cancel";
+  task.appendChild(cancelIcon)
+  return task
+}
+
+const addToDoItem = () => {
+  let todo = createToDoElm();
+  todoContainer?.appendChild(todo);
+  closeForm();
 }
 
 openFormBtn?.addEventListener("click", openForm)
 closeFormBtn?.addEventListener("click", closeForm)
 pushTaskBtn?.addEventListener("click", addToDoItem)
 
+todoItems.forEach((item: any) => {
+  item.array.forEach((element: any) => {
+    console.log("jus checking", element);
+  });
+})
 
