6149d9929b66f0f5a0642cb168c9166459316a36
jarryingnut <rabhishekranjan@gmail.com>
2022-09-30 16:36:43 +0530

    seperate DOM logic from OOP logic

--- src/HowYouDoin.ts
+++ src/HowYouDoin.ts
@@ -4,9 +4,11 @@ export default class HowYouDoin {
     projects: {name: string, tasks: any[]}[]
     constructor() {
       this.projects = []
-      this.projects.push(new Project('All Tasks'))
+      this.projects.push(new Project('Inbox'))
       this.projects.push(new Project('Today'))
       this.projects.push(new Project('Upcoming'))
       this.projects.push(new Project('Important'))
     }
+
+    
 }
